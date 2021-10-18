1b0718b405b4ca6fca61040e6e19fd53b56b0704
yusaAkin <yusa.akin.03@gmail.com>
2021-10-19 01:05:25 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,25 @@
+// DERS1 JavaScript Temelleri
+console.log("Merhaba Kodlama.io")
+// JS type safe değildir
+let dolarDun = 9.20
+let dolarBugun = 9.30
+dolarDun = "9.20"
+{
+    let dolarDun = 9.10
+}
+console.log(dolarDun);
+
+const euroDun = 11.2
+// euroDun = 11 !HATA
+console.log(euroDun)
+
+// array
+// camelCasing
+// PascalCasing
+let konutKredileri = ["Konut Kredisi", "Emlak Konut Kredisi", "Kamu Konut Kredisi"]
+console.log("<ul>")
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+console.log(konutKredileri)

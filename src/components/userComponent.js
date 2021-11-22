86db1a7921cdd863472cad27be68846f7f6c025b
yusaAkin <yusa.akin.03@gmail.com>
2021-11-22 23:44:49 +0300

    22 Kasım 2021

--- src/components/userComponent.js
+++ src/components/userComponent.js
@@ -1,8 +1,12 @@
+// console.log("User component yüklendi")
+// let customer = {id:1, firstName:"Engin"}
+// customer.lastName = "Demiroğ"
+// console.log(customer.lastName)
+
 import { Baselogger, Elasticlogger, MongoLogger } from "../crossCuttingConcerrs/logging/logger.js"
+import Customer from "../models/customer.js"
 import User from "../models/user.js"
 import UserService from "../services/userService.js"
-
-// console.log("User component yüklendi")
 
 let logger1 = new Elasticlogger()
 let userService = new UserService(logger1)
@@ -11,18 +15,15 @@ let user2 = new User(2,"Baran","Gökçekli","Muğla")
 
 userService.add(user1)
 userService.add(user2)
-console.log(userService.list())
-console.log(userService.getById(2))
+// console.log(userService.list())
+// console.log(userService.getById(2))
+console.log("-----------------------")
+userService.load()
 
-
-
-
-
-
-
-
-
-
-// let customer = {id:1, firstName:"Engin"}
-// customer.lastName = "Demiroğ"
-// console.log(customer.lastName)
+let customerToAdd = new Customer(1,"Seda","Yılmaz","Ankara","adsgasgasg")
+customerToAdd.type = "customer"
+userService.add(customerToAdd)
+console.log(userService.customers)
+console.log(userService.employees)
+console.log(userService.errors)
+console.log(userService.getCustomerSorted())
